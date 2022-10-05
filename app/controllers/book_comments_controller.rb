fa021e70d6f68d28662a22dcb7030f13e5404762
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-10-05 23:56:20 +0000

    [Fisnih] notcommunication

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,21 +1,23 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(book_comment_params)
-    book_comment.book_id = book.id
-    book_comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comments = @book.book_comments
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
+    # redirect_to book_path(book)
   end
   def destroy
-    book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.find_by(params[:book_id])
-    book_comment.destroy
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comments = @book.book_comments
+    @book_comment = current_user.book_comments.find_by(params[:book_id])
+    @book_comment.destroy
+    # redirect_to book_path(book)
   end
-  
+
   private
   def book_comment_params
      params.require(:book_comment).permit(:comment)
   end
-  
+
 end
